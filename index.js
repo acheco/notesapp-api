83e82ac2aab3c5ec19ea57cbc2fd36550e93fa4d
acheco <80710861+acheco@users.noreply.github.com>
2024-09-04 19:45:23 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTML protocol",
+        important: true
+    }
+]
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map((note) => note.id))
+        : 0
+    return maxId + 1;
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+})
+
+// GET all notes
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
+
+//  GET a single note
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const note = notes.find(note => note.id === id);
+
+    if (!note) {
+        res.status(404).send(`A note with the id ${id} does not exist`);
+    }
+    res.json(note);
+});
+
+//  POSTING a note
+app.post('/api/notes', (req, res) => {
+    const body = req.body;
+
+    if (!body.content) {
+        return res.status(400).json({
+            error: 'No content provided'
+        })
+    }
+
+    const note = {
+        id: generateId(),
+        content: body.content,
+        important: Boolean(body.important) || false,
+    }
+
+    notes = notes.concat(note);
+
+    res.json(body);
+})
+
+//  DELETE a note
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const note = notes.find(note => note.id === id);
+    if (!note) {
+        res.status(404).send(`A note with the id ${id} dont exist or was already deleted`);
+    }
+    res.status(204).end();
+})
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
